c8def6981f4ecfdefdd950e1675fddb4f17e8018
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-29 22:59:02 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+  
+  
+  def index
+  end
+  
+  def add
+    @cart = cart
+    @cart << params[:product]
+    render :index
+  end
+  
+  
+  
+end
